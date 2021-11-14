19f04099a6d4d1fe1ea92c3d9cf4df9f29957729
Renia Kilikian <rkilikian@gmail.com>
2021-11-14 22:50:57 +0200

    Remove unused dependencies and imports

--- java-path-spring-smdb/src/main/java/gr/codelearn/spring/assignment/smdb/app/Application.java
+++ java-path-spring-smdb/src/main/java/gr/codelearn/spring/assignment/smdb/app/Application.java
@@ -2,10 +2,8 @@ package gr.codelearn.spring.assignment.smdb.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(namedQueriesLocation = "classpath:jpa-named-queries.properties")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
